6e731ba79a0c5ca878d197a3cc2edd25d72cb95a
eagle840 <eagle840@gmail.com>
2020-02-23 11:06:45 +0000

    gen code

---
+++ python-flask-server/swagger_server/controllers/default_controller.py
@@ -0,0 +1,76 @@
+import connexion
+import six
+
+from swagger_server.models.people import People  # noqa: E501
+from swagger_server.models.person import Person  # noqa: E501
+from swagger_server.models.person_data import PersonData  # noqa: E501
+from swagger_server import util
+
+
+def people_add(person):  # noqa: E501
+    """Add a person to the database
+
+     # noqa: E501
+
+    :param person: 
+    :type person: dict | bytes
+
+    :rtype: Person
+    """
+    if connexion.request.is_json:
+        person = PersonData.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def people_list():  # noqa: E501
+    """Get a list of all people
+
+     # noqa: E501
+
+
+    :rtype: People
+    """
+    return 'do some magic!'
+
+
+def person_delete(uuid):  # noqa: E501
+    """Delete this person
+
+     # noqa: E501
+
+    :param uuid: 
+    :type uuid: 
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def person_get(uuid):  # noqa: E501
+    """Get information about one person
+
+     # noqa: E501
+
+    :param uuid: 
+    :type uuid: 
+
+    :rtype: Person
+    """
+    return 'do some magic!'
+
+
+def person_update(uuid, person):  # noqa: E501
+    """Update information about one person
+
+     # noqa: E501
+
+    :param uuid: 
+    :type uuid: 
+    :param person: 
+    :type person: dict | bytes
+
+    :rtype: Person
+    """
+    if connexion.request.is_json:
+        person = PersonData.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
