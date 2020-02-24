a35c0b439d96aee380a93b613d5bef12356202bf
eagle840 <eagle840@gmail.com>
2020-02-24 09:36:37 +0000

    general sync

--- python-flask-server/swagger_server/controllers/default_controller.py
+++ python-flask-server/swagger_server/controllers/default_controller.py
@@ -12,7 +12,8 @@ mydb = mysql.connector.connect(
    host="localhost",
    user="root",
    passwd="1234",
-   auth_plugin="mysql_native_password"
+   auth_plugin="mysql_native_password",
+   database="titanic"
    )
 
 # print(mydb)
