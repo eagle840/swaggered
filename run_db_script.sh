925f68a9d6a0aca5b3ee22d86c0364783f1cc0a2
ir4engineer <39741327+eagle840@users.noreply.github.com>
2020-02-24 11:51:52 +0000

    Update run_db_script.sh

--- run_db_script.sh
+++ run_db_script.sh
@@ -7,16 +7,18 @@ docker cp dbtx:/etc/mysql/my.cnf .
 cat add.txt >> my.cnf
 docker cp my.cnf dbtx:/etc/mysql/.
 
-echo copy db script  and csv over
+echo copy files to dbtx
 
 docker cp db_script.sql dbtx:/.
 docker cp titanic.csv dbtx:/.
+docker cp runsql.sh dbtx:/.
 echo restarting mysql server dbtx
 docker restart dbtx
 
 echo running db input script
 # docker exec -it dbtx mysql < db_script.sql
-docker exec -d dbtx  /usr/bin/mysql -uroot -p1234 < db_script.sql
+# [failed] docker exec -d dbtx  /usr/bin/mysql -uroot -p1234 < db_script.sql
+docker exec -d dbtx /bin/sh -c ./runsql.sh
 
 echo cmd prompt sql server
 docker exec -it dbtx /bin/bash
