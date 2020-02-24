a4a8aec2a96cd741ece33de0c923c1a5fd0ebf94
ir4engineer <39741327+eagle840@users.noreply.github.com>
2020-02-24 11:34:09 +0000

    Update run_db_script.sh

--- run_db_script.sh
+++ run_db_script.sh
@@ -1,20 +1,23 @@
+echo pulling/running sql
 docker run -d --name dbtx -e MYSQL_ROOT_PASSWORD=1234 mysql
 sleep 10
 
-# change the my.cnf to allow LOAD DATA INLINE
+echo change the my.cnf to allow LOAD DATA INLINE
 docker cp dbtx:/etc/mysql/my.cnf .
 cat add.txt >> my.cnf
 docker cp my.cnf dbtx:/etc/mysql/.
 
-# copy db script  and csv over
+echo copy db script  and csv over
 
 docker cp db_script.sql dbtx:/.
 docker cp titanic.csv dbtx:/.
-
+echo restarting mysql server dbtx
 docker restart dbtx
 
+echo running db input script
 docker exec -it dbtx mysql < db_script.sql
 
+echo cmd prompt sql server
 docker exec -it dbtx /bin/bash
 
 
